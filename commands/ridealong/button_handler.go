7015a884fca644ea82d0fa09640c81d9dde8bef7
bluejutzu <kalebdaniel98@gmail.com>
2025-04-26 14:26:37 +0200

    redundant functions

--- commands/ridealong/button_handler.go
+++ commands/ridealong/button_handler.go
@@ -72,38 +72,3 @@ func HandleButtons(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	})
 }
 
-func showRequestModal(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	modal := &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseModal,
-		Data: &discordgo.InteractionResponseData{
-			CustomID: "ra_request_modal",
-			Title:    "Ride Along Request",
-			Components: []discordgo.MessageComponent{
-				discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						discordgo.TextInput{
-							CustomID:  "callsign",
-							Label:     "Your Callsign",
-							Style:     discordgo.TextInputShort,
-							Required:  true,
-							MinLength: 2,
-							MaxLength: 10,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	s.InteractionRespond(i.Interaction, modal)
-}
-
-func completeLog(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: "Ride along completed",
-			Flags:   discordgo.MessageFlagsEphemeral,
-		},
-	})
-}
